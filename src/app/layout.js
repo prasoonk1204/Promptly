7787f57ce312efe6ca246bc17d1b55ad46276640
prasoonk1204 <prasoonkumar467@gmail.com>
2025-07-06 22:36:49 +0530

    feat: Add @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Promptly.ai",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={` antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
